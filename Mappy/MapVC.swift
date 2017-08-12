e5b7299a7377b9d9e9845b32f3a27c6e432910b6
Viraj <developer.viraj@gmail.com>
2017-08-11 20:27:13 -0400

    project created

---
+++ Mappy/MapVC.swift
@@ -0,0 +1,25 @@
+//
+//  MapVC.swift
+//  Mappy
+//
+//  Created by Viraj Upadhyay on 8/11/17.
+//  Copyright © 2017 agloe labs. All rights reserved.
+//
+
+import UIKit
+
+class MapVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
